150be94a38871d51185f2621483e8ff77aa7e650
Remy Ochei <do.infinitely@gmail.com>
2025-05-24 09:40:20 -0500

    Adding padding to the phoneme dataset

--- phoneme_net.py
+++ phoneme_net.py
@@ -6,13 +6,15 @@ import numpy as np
 from audio_converter import audio_to_melspectrogram
 import audioread
 import soundfile
-
+import torch
+import math
 
 class PhonemeSet(Dataset):
     def __init__(self):
         self.mel_specs = []
         self.phonemes = []
         self.phones = []
+        self.max_length = 0
         with open('cmudict-0.7b', encoding="ISO-8859-1") as f:
             for line in f:
                 if not line.startswith(';;;'):
@@ -26,6 +28,7 @@ class PhonemeSet(Dataset):
                             continue
                         except soundfile.LibsndfileError:
                             continue
+                        self.max_length = max(self.max_length, len(mel_spec))
                         phoneme = line.split('  ')[1].split()
                         self.mel_specs.append(mel_spec)
                         self.phonemes.append(phoneme)
@@ -34,6 +37,8 @@ class PhonemeSet(Dataset):
                 m = re.match(r'([A-Z]+)\d*', phone)
                 if m.group(1) not in self.phones:
                     self.phones.append(m.group(1))
+        for i,x in enumerate(self.phones):
+            print(i,x)
     def __len__(self):
         return len(self.mel_specs)
 
@@ -43,7 +48,11 @@ class PhonemeSet(Dataset):
             m = re.match(r'([A-Z]+)\d*', phone)
             phonemes[self.phones.index(m.group(1))] = 1
         phonemes = np.array(phonemes)
-        return self.mel_specs[idx], phonemes
+        length = self.mel_specs[idx].shape[1]
+        padding = [(0,0),(math.floor((self.max_length-length)/2),math.ceil((self.max_length-length)/2))]
+        return torch.from_numpy(np.pad(self.mel_specs[idx], padding)), torch.from_numpy(phonemes)
 
-#model = torchvision.models.vgg19_bn()
-
+if __name__ == "__main__":
+    #model = torchvision.models.vgg19_bn()
+    dataset = PhonemeSet()
+    print(dataset[0][0].shape, dataset[1][0].shape, dataset[0][1].shape)
